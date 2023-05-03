17bb35b2ba89d5317009891bf1184af6ec341391
Joey Cos <joeycosentino24@gmail.com>
2023-05-03 17:57:27 -0400

    homepage request

--- db/index.js
+++ db/index.js
@@ -1,6 +1,18 @@
 import express from 'express';
+import mysql from 'mysql';
 
 const app = express();
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'insterlater',
+    database: 'bookstore'
+})
+
+app.get('/', (req, res) => {
+    res.json('hello, backend here')
+})
 
 app.listen(8800, ()=> {
     console.log('Connected to database!');
